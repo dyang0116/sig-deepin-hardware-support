180d14d96a03c9c4d38e9bcc113c52189ace8c66
Dian-Syuan Yang <dian_syuan0116@realtek.com>
2024-01-31 16:27:10 +0800

    Update release driver version
    Change-Id: I1d9fd1a3b89e11dac5240cd20a2474b0024b54b2
    Signed-off-by: Dian-Syuan Yang <dian_syuan0116@realtek.com>

--- backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
+++ backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
@@ -1,3 +1,3 @@
-static const char drv_ver[]="v6.6-backport-5.15-4-g9405c0925";
+static const char drv_ver[]="v6.6.1-backport-5.15-7-gf2b0a729d";
 #include <linux/module.h>
 module_param_string(drv_ver, drv_ver, sizeof(drv_ver), 0444);
