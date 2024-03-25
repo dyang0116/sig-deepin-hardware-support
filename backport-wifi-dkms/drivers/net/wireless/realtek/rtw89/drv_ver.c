411b351ed47995501c578f84829ff26f8b7abb68
Dian-Syuan Yang <dian_syuan0116@realtek.com>
2024-03-25 17:08:51 +0800

    Update release driver version
    Add patch "wifi: rtw89: download firmware with five times retry"
    
    Change-Id: I2139f8fc7a179450775048b50152b13b7d7266b7
    Signed-off-by: Dian-Syuan Yang <dian_syuan0116@realtek.com>

--- backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
+++ backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
@@ -1,3 +1,3 @@
-static const char drv_ver[]="v6.6.1-backport-5.15-7-gf2b0a729d";
+static const char drv_ver[]="v6.6.1-backport-5.15-9-gfcb4f961c";
 #include <linux/module.h>
 module_param_string(drv_ver, drv_ver, sizeof(drv_ver), 0444);
