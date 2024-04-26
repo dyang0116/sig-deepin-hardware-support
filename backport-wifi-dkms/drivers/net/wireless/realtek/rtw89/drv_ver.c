b08870e2b2e03aecd1a8304587fe4c951900de35
Dian-Syuan Yang <dian_syuan0116@realtek.com>
2024-04-26 13:40:23 +0800

    Update release driver version for 8852be and 8852ce
    1. Add some required patch sets for rtw89 8852be and 8852ce
    2. Support rfkill for rtw89 8852ce and 8851be
    
    Change-Id: I14a41f28023945ed0fbb6478bbcf3acb42b919fb
    Signed-off-by: Dian-Syuan Yang <dian_syuan0116@realtek.com>

--- backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
+++ backport-wifi-dkms/drivers/net/wireless/realtek/rtw89/drv_ver.c
@@ -1,3 +1,3 @@
-static const char drv_ver[]="v6.6.1-backport-5.15-9-gfcb4f961c";
+static const char drv_ver[]="v6.8-backport-5.15-43-g606c895b3";
 #include <linux/module.h>
 module_param_string(drv_ver, drv_ver, sizeof(drv_ver), 0444);
