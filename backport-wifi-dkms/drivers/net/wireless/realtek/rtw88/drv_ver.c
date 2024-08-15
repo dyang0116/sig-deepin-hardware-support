7065614d2b952189557f3aa5c1709faa303e9a48
Dian-Syuan Yang <dian_syuan0116@realtek.com>
2024-08-15 14:54:04 +0800

    wifi: rtw88: get release driver version via more convenient method
        We add below methods to show release driver version.
        1. Use modinfo command
        2. Print version in kernel log when loading modules
    
    Change-Id: Iec00970891587d6fb0741bc14932275970911d2f
    Signed-off-by: Dian-Syuan Yang <dian_syuan0116@realtek.com>

--- backport-wifi-dkms/drivers/net/wireless/realtek/rtw88/drv_ver.c
+++ backport-wifi-dkms/drivers/net/wireless/realtek/rtw88/drv_ver.c
@@ -1,3 +1,21 @@
-static char *drv_ver = "v6.8-backport-5.15-5-gafb331b8";
+#define VERSTR "v6.8-backport-5.15-7-g8ebd2d3d"
+
+static char *drv_ver = VERSTR;
 #include <linux/module.h>
 module_param(drv_ver, charp, 0444);
+MODULE_PARM_DESC(drv_ver, VERSTR);
+
+static int __init rtw88_drv_init(void)
+{
+	printk("rtw88 drv init version: %s\n", VERSTR);
+
+	return 0;
+}
+
+static void __exit rtw88_drv_exit(void)
+{
+	printk("rtw88 drv exit\n");
+}
+
+module_init(rtw88_drv_init);
+module_exit(rtw88_drv_exit);
